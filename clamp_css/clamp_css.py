ea482e4e36183fb0cc2a60182d98a87ec6004873
Giuseppe Varas <giuseppe.varas@accenture.com>
2023-01-13 00:39:04 -0300

    wip(get): add getter

--- clamp_css/clamp_css.py
+++ clamp_css/clamp_css.py
@@ -2,18 +2,30 @@
 
 class ClampCss:
     def __init__(self, min_screen: int = 1366, max_screen: int = 1920, fontSize_base: int = 16, decimal: int = 2):
-        self.min_screen = min_screen
-        self.max_screen = max_screen
-        self.fontSize_base = fontSize_base
-        self.decimal = decimal
+        self.__min_screen = min_screen
+        self.__max_screen = max_screen
+        self.__fontSize_base = fontSize_base
+        self.__decimal = decimal
 
-    def transform_value_to_rem(self, value):
-        return f"{round(value / self.fontSize_base, self.decimal)}rem"
+    def get_min_screen(self):
+        return self.__min_screen
 
-    def getClamp(self, min_size, max_size):
+    def get_max_screen(self):
+        return self.__max_screen
+
+    def get_fontSize_base(self):
+        return self.__fontSize_base
+
+    def get_decimal(self):
+        return self.__decimal
+
+    def transform_value_to_rem(self, value) -> str:
+        return f"{round(value / self.__fontSize_base, self.__decimal)}rem"
+
+    def getClamp(self, min_size, max_size) -> str:
         v = round(((100 * (max_size - min_size)) /
-                   (self.max_screen - self.min_screen)), self.decimal)
-        r = ((self.min_screen * max_size) - (self.max_screen * min_size)
-             ) / (self.min_screen - self.max_screen)
+                   (self.__max_screen - self.__min_screen)), self.__decimal)
+        r = ((self.__min_screen * max_size) - (self.__max_screen * min_size)
+             ) / (self.__min_screen - self.__max_screen)
 
         return f"clamp({self.transform_value_to_rem(min_size)}, {v}vw + {self.transform_value_to_rem(r)}, {self.transform_value_to_rem(max_size)})"
